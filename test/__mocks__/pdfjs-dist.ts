f75b51d465dd620fe7380c964079117de0adfcf5
Emmanuel Boisset <manuboisset@gmail.com>
2026-05-12 11:01:10 +0200

    fix: signature function fix

--- test/__mocks__/pdfjs-dist.ts
+++ test/__mocks__/pdfjs-dist.ts
@@ -2,9 +2,17 @@ export const getDocument = jest.fn(() => ({
   promise: Promise.resolve({
     numPages: 1,
     getPage: jest.fn().mockResolvedValue({
-      getTextContent: jest.fn().mockResolvedValue({
-        items: [],
+      getTextContent: jest.fn().mockResolvedValue({ items: [] }),
+      getViewport: jest.fn().mockReturnValue({
+        transform: [1, 0, 0, 1, 0, 0],
+        height: 800,
+        width: 600,
       }),
     }),
   }),
 }));
+
+export const Util = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform: jest.fn((transform, itemTransform) => [1, 0, 0, 1, 0, 0]),
+};
